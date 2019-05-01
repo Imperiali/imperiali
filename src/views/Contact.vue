a6a61b1b26c09706b230e7a99f93ec2b44e80375
Igor Imperiali <igor.fernandes@al.infnet.edu.br>
2019-05-01 10:38:31 -0700

    Adding Contact view

---
+++ src/views/Contact.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="contact">
+    <v-container class="my-5">
+      Contact
+    </v-container>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Contact"
+  }
+</script>
+
+<style scoped>
+
+</style>
