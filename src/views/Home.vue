6cf3fb22a8875bae3e76d8a294d0267ee80f6671
Igor Imperiali <igor.fernandes@al.infnet.edu.br>
2019-05-01 11:50:54 -0700

    Adding content to home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,30 @@
 <template>
   <div class="home">
     <v-container class="my-5">
-      Home
+      <v-layout row wrap align-center>
+
+        <v-flex xs12 sm6 class="avatar text-xs-center">
+          <v-avatar
+            class="elevation-5"
+            size="300">
+            <v-img
+              position="bottom"
+              src="https://lh3.googleusercontent.com/9yA6fme2l7vEtbnwncOyPygnfTy8dz2YZcyJlIpHlApJinBjAEuHMris4Lc8zimMDPzns9gh4fgsEwf1thdo1oqEfWFmMU4q5xB1j2qy96EeqxxrnSmwme1BoDfNCvcKlYMqE5SqKYbGjmT0yYr7flaogstk1RtOJm2fWaUxJpBtW_N6JaLmTcS2sTKBuW3E4cFBTPlUOdnb_3WzBDH92K0vULhXDk8iK-_0Bm501O6J9lGYY269SqQYgQ1fZqAMQlCPq2yECVfI9N6od1jaJiYaz7YuFvJXR7ZV_UEdce0pZCtK06TsYbNDbq8xhXrswFGxyWXg15Gx2LMleGjjvKNpWPCRCVHUKaALT9uFSISZvjXQHoNFuoickiEnJndynzZU-Ap2cBdS71YTxrxb3dZxdadI68DH5FxV1kQZ6vAXjE71KD2FtKu1dDGfFhV9TeVp0sBxGBCnaHn3YgNAEJ-QodBG4v-kERMbZlBw3NpGFffOVQHhTLeP1PQv6NuVoVxgbKu26ydwoqj3eunK54-xQQu0jlDUDKG7lzw97d3C3ke409et9AQt7BtHUnCR34fmgbjwuxKhL6mSRO5XcLu7cmGdSh2DHcdU5m6CCCot_xA5sV3koGG7jsnTiRdAS0C7LMNY0Fiz_y2tWKz2AGQ2OQGh4Is=w498-h622-no"
+              alt=""/>
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs12 sm6 class="presentation ">
+          <h1 class="headline">Olá!</h1>
+          <p class="display-2">Igor <b>Imperiali</b> aqui!</p>
+          <p class="subheading">Desenvolvedor Web</p>
+          <p class="body-1">Estudo Engenharia da computação desde o final de 2017 e trabalho como desenvolvedor desde o começo de 2018.
+            Depois de muitos anos em busca do que realmente me fazia feliz e satisfeito, me encontrei na área do desenvolvimento.
+            Trabalhei por algum tempo no meio da industria após concluir meu Técnico em Mecânica, mas é em computação onde realmente
+            quero estar.</p>
+        </v-flex>
+
+      </v-layout>
     </v-container>
   </div>
 </template>
