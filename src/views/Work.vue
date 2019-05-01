23f1e79263d8e72e5ebd50b17a604e5c3072f670
Igor Imperiali <igor.fernandes@al.infnet.edu.br>
2019-05-01 10:38:20 -0700

    Adding Work view

---
+++ src/views/Work.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="work">
+    <v-container class="my-5">
+      Work
+    </v-container>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Work"
+  }
+</script>
+
+<style scoped>
+
+</style>
