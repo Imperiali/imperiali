18ef567af99a92879c03b2a277a44eaf350e5e50
Igor Imperiali <igor.fernandes@al.infnet.edu.br>
2019-05-01 10:37:33 -0700

    Making the base of the toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,43 @@
 <template>
-  <v-app>
-    <v-toolbar app>
+  <v-app >
+    <v-toolbar flat>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <router-link to="/" tag="span">
+          <span>Imperiali</span>
+        </router-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+      <router-link to="/about" tag="v-btn">
+        <v-btn flat >
+          <span class="mr-2">About</span>
+        </v-btn>
+      </router-link>
+      <router-link to="/work" tag="v-btn">
+        <v-btn flat >
+          <span class="mr-2">Work</span>
+        </v-btn>
+      </router-link>
+      <router-link to="/contact" tag="v-btn">
+        <v-btn flat >
+          <span class="mr-2">Contact</span>
+        </v-btn>
+      </router-link>
     </v-toolbar>
 
-    <v-content>
-      <HelloWorld/>
+    <v-content class="mx-4 mb-4">
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
-    return {
-      //
+  export default {
+    name: 'App',
+    data () {
+      return {
+        //
+      }
     }
   }
-}
 </script>
